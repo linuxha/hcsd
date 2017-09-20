7af4deea7d8ba239081410c90f91dc6fa340fa91
Neil Cherry <linuxha@linuxha.com>
2017-09-19 21:35:12 -0400

    Did some reformatting of the code.
    Odd, I recall fixing an indexing problem on the buf[] array but that
    doesn't show up in the git diff. Just the reformatting of the code.

--- hcsclock.c
+++ hcsclock.c
@@ -52,7 +52,7 @@ extern int interval;
 
 uint
 bcd2b(uint v) {
-  return(((v>>4)*10)+(v&0xf));
+    return(((v>>4)*10)+(v&0xf));
 }
 
 /*
@@ -61,69 +61,70 @@ bcd2b(uint v) {
 
 uint
 b2bcd(uint v) {
-  return(((v/10)<<4)|(v%10));
+    return(((v/10)<<4)|(v%10));
 }
 
 void
 sethcsclock(int device) {
-  int i;
+    int i;
   
-  time_t t;
-  struct tm *tms;
+    time_t t;
+    struct tm *tms;
   
-  char tmp[MAXLINE];
-  char buf[10];
+    char tmp[MAXLINE];
+    char buf[10];
 
-  /*
-  ** First we need to tell the HCS how often to update to update the time
-  */
+    /*
+    ** First we need to tell the HCS how often to update to update the time
+    */
   
-  buf[0] = CMD;                 /*  */
-  buf[1] = SELECTTIME;
-  buf[2] = interval;
+    buf[0] = CMD;                 /*  */
+    buf[1] = SELECTTIME;
+    buf[2] = interval;
 
-  if((i = write(device, buf, 3)) != 3) { /* OK for now I'll do nothing on error */
-    syslog(LOG_ERR, "Write error (%d) != 9 (Err: %c)", i, errno);
-    return;
-  }
+    if((i = write(device, buf, 3)) != 3) { /* OK for now I'll do nothing on error */
+	syslog(LOG_ERR, "Write error (%d) != 9 (Err: %c)", i, errno);
+	return;
+    }
   
-  /*
-  ** Here is where we should wait to get the time back if interval is not OFF
-  ** This gives us a way of telling weather we are talking to the HCS.
-  */
+    /*
+    ** Here is where we should wait to get the time back if interval is not OFF
+    ** This gives us a way of telling whether we are talking to the HCS.
+    */
   
-  if(interval) {
-    ;
-  }
+    if(interval) {
+	;
+    }
   
-  buf[0] = CMD;                 /* ! */
+    buf[0] = CMD;                 /* ! */
   
-  if((i = write(device, buf, 1)) != 1) { /* OK for now I'll do nothing on error */
-    syslog(LOG_ERR, "Write error (%d) != 9 (Err: %c)", i, errno);
-    return;
-  }
+    if((i = write(device, buf, 1)) != 1) { /* OK for now I'll do nothing on error */
+	syslog(LOG_ERR, "Write error (%d) != 9 (Err: %c)", i, errno);
+	return;
+    }
   
-  time(&t);
-  tms    = localtime(&t);
-  buf[0] = SETTIME;
-  buf[1] = 0;                     /* Hundredths of seconds */
-  buf[2] = b2bcd(tms->tm_sec);    /* seconds               */
-  buf[3] = b2bcd(tms->tm_min);    /* minutes               */
-  buf[4] = b2bcd(tms->tm_hour);   /* hours                 */
-  buf[5] = b2bcd(tms->tm_wday+1); /* day of the week       */
-  buf[6] = b2bcd(tms->tm_mday);   /* day of the month      */
-  buf[7] = b2bcd(tms->tm_mon+1);  /* month                 */
-  buf[8] = b2bcd(tms->tm_year);   /* year (2 digits)       */
-  buf[9] = 0;
+    time(&t);
+    tms    = localtime(&t);
+    buf[0] = SETTIME;
+    buf[1] = 0;                     /* Hundredths of seconds */
+    buf[2] = b2bcd(tms->tm_sec);    /* seconds               */
+    buf[3] = b2bcd(tms->tm_min);    /* minutes               */
+    buf[4] = b2bcd(tms->tm_hour);   /* hours                 */
+    buf[5] = b2bcd(tms->tm_wday+1); /* day of the week       */
+    buf[6] = b2bcd(tms->tm_mday);   /* day of the month      */
+    buf[7] = b2bcd(tms->tm_mon+1);  /* month                 */
+    buf[8] = b2bcd(tms->tm_year);   /* year (2 digits)       */
+    buf[9] = 0;
 
-  sprintf(tmp,"Time set to: %04u/%02u/%02u %02u:%02u:%02u\n",
-          tms->tm_year+1900, tms->tm_mon+1, tms->tm_mday,
-          tms->tm_hour,      tms->tm_min,   tms->tm_sec);
+    sprintf(tmp,"Time set to: %04u/%02u/%02u %02u:%02u:%02u\n",
+	    tms->tm_year+1900, tms->tm_mon+1, tms->tm_mday,
+	    tms->tm_hour,      tms->tm_min,   tms->tm_sec);
 
-  syslog(LOG_INFO, tmp);
+    syslog(LOG_INFO, tmp);
 
-  if((i = write(device, buf, 9)) != 9) { /* OK for now I'll do nothing on error */
-    syslog(LOG_ERR, "Write error (%d) != 9 (Err: %c)", i, errno);
-  }
+    if((i = write(device, buf, 9)) != 9) { /* OK for now I'll do nothing on error */
+	syslog(LOG_ERR, "Write error (%d) != 9 (Err: %c)", i, errno);
+	return ;
+    }
 }
 
